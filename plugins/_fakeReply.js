0af2cea0674a490116201951dea34f9d98f8bce7
MauroAzcurra <137190861+MauroAzcurra@users.noreply.github.com>
2024-09-23 15:02:24 -0300

    Add files via upload

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,9 +1,32 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-let img = await (await fetch(`https://f.uguu.se/GOcJfkyz.jpg`)).buffer()
+let img = await (await fetch(`https://i.pinimg.com/originals/79/69/bd/7969bd5faa4d1fa5193d0e30bb4e974c.jpg`)).buffer()
 
- global.rcanal = {
+global.rcanal = {
+ contextInfo: {
+     	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363319490414132@newsletter",
+      serverMessageId: 100,
+      newsletterName: namechannel,
+   }, 
+   externalAdReply: {
+    showAdAttribution: true, 
+    title: botname, 
+    body: textbot, 
+    mediaUrl: null, 
+    description: null, 
+    previewType: "PHOTO", 
+    thumbnailUrl: "https://i.pinimg.com/originals/79/69/bd/7969bd5faa4d1fa5193d0e30bb4e974c.jpg", 
+    sourceUrl: canal, 
+    mediaType: 1, 
+    renderLargerThumbnail: false }, 
+    }, 
+    }
+
+
+/* global.rcanal = {
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
@@ -12,7 +35,7 @@ let img = await (await fetch(`https://f.uguu.se/GOcJfkyz.jpg`)).buffer()
       newsletterName: namechannel,
     },
     },
-  }
+  }*/
 
  global.adReply = {
 	    contextInfo: { 
@@ -25,7 +48,7 @@ let img = await (await fetch(`https://f.uguu.se/GOcJfkyz.jpg`)).buffer()
 					mediaUrl: null,
 					description: null,
 					previewType: "PHOTO",
-					thumbnailUrl: img,
+					thumbnailUrl: "https://i.ibb.co/8mdHFVK/file.jpg",
                     thumbnail: img,
 		           sourceUrl: canal,
 		           mediaType: 1,
